f653fda94e9c6c25db5c6453153fb013822460c7
huangxiaoxun <hxx094438@gmail.com>
2023-03-11 14:39:04 +0800

    release project

--- coco.config.js
+++ coco.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'coco-template',
   templateName: '营销活动',
-  author: 'muwoo',
+  author: 'hxx',
   snapshot: 'https://cdn.img.snapshot.png',
-  gitUrl: 'coco-h5/coco-template',
+  gitUrl: 'hxxlowcode/coco-template',
   projectType: '0'
 };
